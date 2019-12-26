f897bfea35990aaea6be04d9c443b0ce1fe099fd
EtienneDel <etienne.delnott@viacesi.fr>
2019-12-26 13:02:10 +0100

    Modified app.tsx and should have added prettier

--- cookingMain/App.tsx
+++ cookingMain/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+    const [state, setState] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{state}</Text>
+      <Button title="BONJOUR" onPress={() => setState("BONJOUR")}/>
+      <Button title="AUREVOIR" onPress={() => setState("BONSOIR")}/>
     </View>
+
   );
 }
 
