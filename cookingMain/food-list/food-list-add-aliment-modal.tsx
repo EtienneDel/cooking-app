3cc468d6eacf3f1af2137db9e8c8819ad8554562
Etienne <etienne.delnott@viacesi.fr>
2020-01-06 22:17:08 +0100

    WIP: Food list modal

---
+++ cookingMain/food-list/food-list-add-aliment-modal.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { AlimentsModel, AlimentsType } from "../models/aliments-model";
+import { Button, Modal, StyleSheet, Text, TextInput, View } from "react-native";
+
+interface props {
+  foodList: AlimentsModel[];
+  isVisibleParent: boolean;
+}
+
+export default function FoodListAddAlimentModal({
+  foodList,
+  isVisibleParent
+}: props) {
+  const [modalForm, setModalForm] = useState<AlimentsModel>({
+    type: AlimentsType.FRUIT,
+    name: ""
+  });
+  const [isVisible, setIsVisible] = useState(isVisibleParent);
+
+  return (
+    <Modal animationType="slide" visible={isVisible} transparent={true}>
+      <View style={styles.modal}>
+        <Text>Aliment type</Text>
+        <TextInput
+          style={styles.textInput}
+          defaultValue={modalForm.type}
+          autoFocus={true}
+        />
+        <Text>Aliment name</Text>
+        <TextInput
+          style={styles.textInput}
+          defaultValue={modalForm.name}
+          onChangeText={e => setModalForm({ name: e, type: modalForm.type })}
+        />
+        <Button
+          title={"Validate"}
+          onPress={() => {
+            foodList.push(modalForm);
+            setIsVisible(false);
+          }}
+        />
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00000080"
+  },
+  textInput: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 1
+  }
+});
--- cookingMain/food-list/food-list.tsx
+++ cookingMain/food-list/food-list.tsx
@@ -10,77 +10,46 @@ import {
 } from "react-native";
 import { AlimentsModel, AlimentsType } from "../models/aliments-model";
 import { fruitsFake, vegetablesFake } from "../data-sets/aliments-dataset";
+import FoodListAddAlimentModal from "./food-list-add-aliment-modal";
 
 export default function FoodList() {
   const [fruitList, setFruitList] = useState<AlimentsModel[]>(fruitsFake);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [modalForm, setModalForm] = useState<AlimentsModel>({
-    type: AlimentsType.FRUIT,
-    name: ""
-  });
 
   return (
-    <>
-      <View style={styles.container}>
-        <SectionList
-          sections={[
-            { title: AlimentsType.FRUIT, data: fruitList },
-            { title: AlimentsType.VEGETABLE, data: vegetablesFake }
-          ]}
-          renderItem={({ item }) => (
-            <Text
-              style={styles.aliments}
-              onPress={() =>
-                setFruitList(
-                  fruitList.filter(fruit => fruit.name !== item.name)
-                )
-              }
-            >
-              {item.name}
-            </Text>
+    <View style={styles.container}>
+      <SectionList
+        sections={[
+          { title: AlimentsType.FRUIT, data: fruitList },
+          { title: AlimentsType.VEGETABLE, data: vegetablesFake }
+        ]}
+        renderItem={({ item }) => (
+          <Text
+            style={styles.aliments}
+            onPress={() =>
+              setFruitList(fruitList.filter(fruit => fruit.name !== item.name))
+            }
+          >
+            {item.name}
+          </Text>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+        keyExtractor={(item, index) => item.name + Math.random()}
+      />
+      <View style={styles.button}>
+        <Button
+          title={"Add aliment"}
+          onPress={() => (
+            <FoodListAddAlimentModal
+              foodList={fruitList}
+              isVisibleParent={!showModal}
+            />
           )}
-          renderSectionHeader={({ section }) => (
-            <Text style={styles.sectionHeader}>{section.title}</Text>
-          )}
-          keyExtractor={(item, index) => item.name + Math.random()}
         />
-        <View style={styles.button}>
-          <Button
-            title={"Add aliment"}
-            onPress={() => {
-              setShowModal(true);
-            }}
-          />
-        </View>
       </View>
-      <View style={styles.modal}>
-        <Modal animationType="slide" visible={showModal} transparent={false}>
-          <View>
-            <Text>Aliment type</Text>
-            <TextInput
-              style={styles.textInput}
-              defaultValue={modalForm.type}
-              autoFocus={true}
-            />
-            <Text>Aliment name</Text>
-            <TextInput
-              style={styles.textInput}
-              defaultValue={modalForm.name}
-              onChangeText={e =>
-                setModalForm({ name: e, type: modalForm.type })
-              }
-            />
-            <Button
-              title={"Validate"}
-              onPress={() => {
-                fruitList.push(modalForm);
-                setShowModal(false);
-              }}
-            />
-          </View>
-        </Modal>
-      </View>
-    </>
+    </View>
   );
 }
 
@@ -88,8 +57,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    marginTop: 50,
-    alignItems: "center"
+    paddingTop: 50,
+    paddingBottom: 50,
+    alignItems: "center",
+    alignSelf: "stretch",
+    backgroundColor: "#F235B0"
   },
   sectionHeader: {
     textAlign: "center",
@@ -101,7 +73,7 @@ const styles = StyleSheet.create({
     paddingBottom: 2,
     fontSize: 32,
     fontWeight: "bold",
-    backgroundColor: "rgba(247,247,247,1.0)",
+    backgroundColor: "#D99E89",
     alignContent: "center"
   },
   aliments: {
@@ -112,20 +84,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 5,
     marginBottom: 5,
-    backgroundColor: "#99D0F2",
+    backgroundColor: "#91E0F2",
     borderRadius: 5
   },
   button: {
-    marginBottom: 10
-  },
-  modal: {},
-  textInput: {
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    borderColor: "black",
-    borderWidth: 2,
-    borderRadius: 1
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderRadius: 5
   }
 });
